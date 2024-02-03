62be2203d6570715c2fc4afce1dc13860a0eab26
Takahiro <tkhr_621@docomo.ne.jp>
2024-02-04 01:35:22 +0900

    useQuery から返ってくる data に方推論が効くようにする

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
 import './App.css'
 import { useQuery } from '@tanstack/react-query'
+import { Posts } from './types'
 
 function App() {
-  const fetchPosts = async () => {
+  const fetchPosts = async (): Promise<Posts> => {
     const res = await fetch('https://jsonplaceholder.typicode.com/posts')
     return res.json()
   }
