42e99994665328cefa6506392234bd1b62e79a88
Takahiro <tkhr_621@docomo.ne.jp>
2024-02-04 16:26:55 +0900

    useMutation をつかう

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import './App.css'
-import { useQuery } from '@tanstack/react-query'
-import { Posts } from './types'
+import { useQuery, useMutation } from '@tanstack/react-query'
+import { Post, Posts } from './types'
 
 function App() {
   const fetchPosts = async (): Promise<Posts> => {
@@ -15,9 +15,29 @@ function App() {
 
   console.log(data)
 
+  const { mutate } = useMutation({
+    mutationFn: async (newPost: Post) => {
+      return await fetch('https://jsonplaceholder.typicode.com/posts', {
+        method: 'POST',
+        body: JSON.stringify(newPost),
+      })
+    },
+  })
+
+  const handleClick = () => {
+    console.log('click')
+    mutate({
+      userId: 101,
+      id: 101,
+      title: 'test',
+      body: 'testtesttest',
+    })
+  }
+
   return (
     <>
       <h1>tanstack-query-demo</h1>
+      <button onClick={handleClick}>POST</button>
     </>
   )
 }
